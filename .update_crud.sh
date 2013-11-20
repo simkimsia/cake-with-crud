4cfc5ad2258f3af1118cb1f2d754f68e5f8f787a
kimsia <kimcity@gmail.com>
2013-11-20 19:19:50 +0800

    Add in bash script that will update the AppController appropriately for crud

--- .update_crud.sh
+++ .update_crud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # remove any traces of crud before we clone
-rm -Rf plugins/crud
+rm -Rf plugins/Crud
 
 # clone the latest cakephp master branch
-git clone git@github.com:FriendsOfCake/crud.git plugins/crud
+git clone git@github.com:FriendsOfCake/crud.git plugins/Crud
